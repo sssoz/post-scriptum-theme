3a3466a32b9db941973da411b8ffdcfedd2f9894
Patrick Boivin <pboivin@gmail.com>
2016-07-26 19:55:58 -0400

    Header: Update page title format

--- header.php
+++ header.php
@@ -4,7 +4,13 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?php wp_title(' '); ?> | <?php bloginfo(' '); ?></title>
+  <title><?php 
+    if (! is_home()) {
+      wp_title(' ');
+      echo ' | ';
+    }
+    bloginfo(' ');
+  ?></title>
   <meta name="description" content="Revue de recherche interdisciplinaire en texte et médias">
   <meta name="keywords" content="Post-Scriptum, littérature comparée, ">
   <meta name="twitter:card" content="summary">

576b7a0a747052506a6e2c49c8a7ab5fb9823dda
Patrick Boivin <pboivin@gmail.com>
2016-07-26 22:11:53 -0400

    Actualites: Fix date display

--- archive-actualites.php
+++ archive-actualites.php
@@ -12,21 +12,22 @@
         'post_status' => 'publish',
       );
       $loop = new WP_Query( $args );
-      if ( $loop -> have_posts() ) : while ( $loop -> have_posts() ) : $loop -> the_post(); ?>
+      if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
       <article class="row" id="post-<?php the_ID(); ?>">
         <div class="col-lg-8 col-lg-offset-2 col-md-6 col-md-offset-3">
           <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
           <h4 class="meta"><a href="<?php the_permalink(); ?>"><?php the_field( "news_subtitle" ); ?></a></h4>
-          <div class="meta meta-date"><?php the_date(); ?></div>
+          <div class="meta meta-date"><?= get_the_date() ?></div>
           <div class="img-container">
             <a href="<?php the_permalink(); ?>">
               <?php
-              $image = get_field('news_img');
-              if( !empty($image) ): ?>
-              <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="img-responsive"/>
-            <?php endif; ?>
-          </a>
+                $image = get_field('news_img');
+                if( !empty($image) ):
+              ?>
+                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="img-responsive"/>
+              <?php endif; ?>
+            </a>
           </div>
           <hr/>
         </div>

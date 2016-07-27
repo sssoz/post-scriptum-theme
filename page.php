83dcb8115733b310ea77a0d8721f33e9a823ba50
Patrick Boivin <pboivin@gmail.com>
2016-07-26 20:54:45 -0400

    Page: Make a-propos the default page layout

--- page.php
+++ page.php
@@ -1,7 +1,15 @@
-  <?php get_header(); ?>
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <?php the_content(); ?>
-  <?php endwhile; else : ?>
-	<p><?php _e( 'Sorry, no pages matched your criteria.' ); ?></p>
-  <?php endif; ?>
-  <?php get_footer(); ?>
+<?php get_header(); ?>
+
+<main id="main" class="container-fluid">
+  <h1><?php echo get_the_title(); ?></h1>
+  <hr/>
+  <article class="col-md-6 col-md-offset-3 single-col">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php the_content(); ?>
+    <?php endwhile; else : ?>
+      <p><?php _e( 'Sorry, no pages matched your criteria.' ); ?></p>
+    <?php endif; ?>
+  </article>
+</main>
+
+<?php get_footer(); ?>

23d8dd8c459c8a929b09bffdda461fe4ccd2cb8b
Patrick Boivin <pboivin@gmail.com>
2016-09-06 17:59:50 -0400

    Basic template for single posts of Entretiens + Comptes rendus

---
+++ single-comptes-rendus.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<main id="main" class="container-fluid">
+  <hr/>
+  <article class="col-md-6 col-md-offset-3 single-col">
+    <h1><?= get_the_title(); ?></h1>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <div class="meta meta-author"><?php the_field('article_author'); ?></div>
+      <div class="meta meta-affiliation"><?php the_field('article_affiliation'); ?></div>
+      <div class="meta meta-date"><?= get_the_date(); ?></div>
+      <br>
+      <?php the_content(); ?>
+    <?php endwhile; else : ?>
+      <p><?php _e( 'Sorry, no pages matched your criteria.' ); ?></p>
+    <?php endif; ?>
+  </article>
+</main>
+
+<?php get_footer(); ?>

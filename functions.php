ebe220e0ba7afb5b9898e1064cba6c59f9b3406a
Patrick Boivin <pboivin@gmail.com>
2016-09-06 15:16:25 -0400

    Add temporary stylesheet

--- functions.php
+++ functions.php
@@ -23,6 +23,7 @@
       /* add styles */
       wp_enqueue_style( 'bootstrap-core', get_template_directory_uri() . '/css/bootstrap.min.css' );
       wp_enqueue_style( 'custom', get_template_directory_uri() . '/style.css' );
+      wp_enqueue_style( 'custom-extra', get_template_directory_uri() . '/style-extra.css' );
 
       /* add scripts */
       wp_enqueue_script( 'colourbrightness', get_template_directory_uri() . '/js/jquery.colourbrightness.min.js', array('jquery'), true );

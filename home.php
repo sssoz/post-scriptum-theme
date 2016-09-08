40241ab054ccc5e9579e5f4baf3c970068c5e5c2
Patrick Boivin <pboivin@gmail.com>
2016-09-07 21:35:53 -0400

    Home: Add fallback cover

--- home.php
+++ home.php
@@ -4,44 +4,51 @@
 
   <!-- Dernière parution -->
 
-  <?php
-    $args = array(
-      'post_type' => 'parutions',
-      'post_status' => 'publish',
-      'posts_per_page' => 1,
-#     'offset' => 0,
-    );
-    $loop = new WP_Query( $args );
-    if ( $loop->have_posts() ) : 
-      $loop->the_post();
-      $img = get_field('series_photo');
-      $bg = 'background: url('.$img['url'].')';
-      ?>
+  <section class="cover">
+    <?php
+      $args = array(
+        'post_type' => 'parutions',
+        'post_status' => 'publish',
+        'posts_per_page' => 1,
+        'offset' => 0,
+      );
+      $loop = new WP_Query( $args );
 
-      <section class="cover">
-        <div class="jumbotron text-center" style="<?= $bg ?>">
-          <div class="jumbo-overlay">
-            <div class="container jumbo-container">
-              <div class="jumbo-latest">
-                <div class="meta">Parution <span class="meta-number">n° <?= the_field('series_number') ?></span></div>
-                <h1><a href="<?= the_permalink() ?>"><?= the_title() ?></a></h1>
-                <div class="meta meta-date"><?= get_the_date('F Y') ?></div>
-                <div class="nav-down">
-                  <a href="#tagline">
-                    <span class="icon-arrow-down"></span>
-                  </a>
+      if ( $loop->have_posts() ) : 
+        $loop->the_post();
+        $img = get_field('series_photo');
+        $bg = 'background: url('.$img['url'].')';
+        ?>
+
+          <div class="jumbotron text-center" style="<?= $bg ?>">
+            <div class="jumbo-overlay">
+              <div class="container jumbo-container">
+                <div class="jumbo-latest">
+                  <div class="meta">Parution <span class="meta-number">n° <?= the_field('series_number') ?></span></div>
+                  <h1><a href="<?= the_permalink() ?>"><?= the_title() ?></a></h1>
+                  <div class="meta meta-date"><?= get_the_date('F Y') ?></div>
+                  <div class="nav-down">
+                    <a href="#tagline">
+                      <span class="icon-arrow-down"></span>
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div id="tagline"><?php bloginfo('description'); ?></div>
-      </section>
+          <div id="tagline"><?php bloginfo('description'); ?></div>
 
-      <?php 
-      wp_reset_postdata();
-    endif;
-  ?>
+        <?php 
+        wp_reset_postdata();
+
+      else : ?>
+
+        <div class="jumbotron" style="height: 200px; background: #7a7a7a;"></div>
+
+        <?php
+      endif;
+    ?>
+  </section>
 
   <!-- Actualités récentes -->
 

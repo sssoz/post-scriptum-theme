8ea37cd1e399b44d3eaa0305b30bd2bfbdebac88
Patrick Boivin <pboivin@gmail.com>
2016-09-08 21:33:27 -0400

    Articles: Base template for single items

--- single.php
+++ single.php
@@ -1,15 +1,67 @@
-<?php get_header(); ?>
+<?php 
+
+$GLOBALS['CUSTOM_TITLE'] = clean_title( get_the_title() );
+
+get_header(); 
+
+$affiliation = get_field('article_affiliation');
+$abstract_fr = get_field('abstract_fr');
+$abstract_en = get_field('abstract_en');
+$references  = get_field('article_references');
+
+?>
 
 <main id="main" class="container-fluid">
-  <h1><?= get_the_title(); ?></h1>
-  <hr/>
-  <article class="col-md-6 col-md-offset-3 single-col">
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-      <?php the_content(); ?>
-    <?php endwhile; else : ?>
-      <p><?php _e( 'Sorry, no pages matched your criteria.' ); ?></p>
-    <?php endif; ?>
-  </article>
+
+<?php if ( have_posts() ) : the_post(); ?>
+
+  <section class="row">
+    <article>
+      <header class="container-fluid">
+        <h1 class="page-title"><?= clean_title( get_the_title() ) ?></h1>
+        <h2 class="meta article-subtitle"><?= get_field('article_subtitle')  ?></h2>
+        <div class="meta"><?= get_field('article_author') ?><?= ($affiliation ? "<br> $affiliation" : "") ?></div>
+      </header>
+
+      <hr/>
+
+      <aside id="article-toc-container" class="col-md-3">
+        <nav id="article-toc" class="col-md-3"></nav>
+      </aside>
+
+      <div id="article-content" class="col-md-6">
+        <div class="text-center">
+          <p class="meta"><small><em>Cet article est apparu dans notre parution n° <?= get_field('series_number') ?>.</em></small></p>
+        </div>
+
+        <?php if ($abstract_fr) : ?>
+          <p><strong>RÉSUMÉ</strong></p>
+          <p><em><?= $abstract_fr ?></em></p>
+        <?php endif; ?>
+
+        <?php if ($abstract_en) : ?>
+          <p><strong>ABSTRACT</strong></p>
+          <p><em><?= $abstract_en ?></em></p>
+        <?php endif; ?>
+
+        <hr />
+
+        <?= get_field('article_text') ?>
+
+        <hr />
+
+        <?php if ($references) : ?>
+          <h3 id="bibliographie">Bibliographie</h3>
+          <?= get_field('article_references') ?>
+        <?php endif; ?>
+
+
+      </div>
+    </article>
+  </section>
+
+<?php endif; ?>
+
 </main>
 
 <?php get_footer(); ?>
